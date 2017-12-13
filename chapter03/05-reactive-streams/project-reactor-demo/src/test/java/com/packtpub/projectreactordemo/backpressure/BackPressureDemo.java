5d3b59e37aad4702ce67dc00601326a29d459e16
Rene Enriquez <enriquezrene.ap@gmail.com>
2017-12-12 23:42:06 -0500

    reactive and data

--- chapter03/05-reactive-streams/project-reactor-demo/src/test/java/com/packtpub/projectreactordemo/backpressure/BackPressureDemo.java
+++ chapter03/05-reactive-streams/project-reactor-demo/src/test/java/com/packtpub/projectreactordemo/backpressure/BackPressureDemo.java
@@ -5,23 +5,20 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class BackPressureDemo {
 
     @Test
-    public void givenAListOfElements_WhenTheBackPressureIsPresent_ThenOnlyTheNElementsRequestedArePresent() throws Exception {
+    public void givenAFluxWith10Elements_WhenBackPressureAsksFor3Elements_ThenTheChunksHaveTheSize_3_3_3_1() throws Exception {
         List<Integer> digitsArray = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-        Iterator<Integer> digitsIterator = digitsArray.iterator();
         Flux<Integer> fluxWithDigits = Flux.fromIterable(digitsArray);
 
-        fluxWithDigits.take(5)
+        fluxWithDigits.buffer(3)
                 .log()
-                .subscribe(element ->
-                        Assert.assertEquals(digitsIterator.next(), element)
+                .subscribe(elements -> {
+                            Assert.assertTrue(elements.size() <= 3);
+                        }
                 );
-
-        Assert.assertTrue(digitsIterator.next().equals(6));
     }
 }
--- chapter03/05-reactive-streams/project-reactor-demo/src/test/java/com/packtpub/projectreactordemo/flux/FluxDemoTest.java
+++ chapter03/05-reactive-streams/project-reactor-demo/src/test/java/com/packtpub/projectreactordemo/flux/FluxDemoTest.java
@@ -2,28 +2,32 @@ package com.packtpub.projectreactordemo.flux;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.function.Function;
 
 public class FluxDemoTest {
 
     @Test
     public void givenAListOfCapitalizedStrings_WhenTheFlatMapConvertsToUpperCaseTheStrings_ThenTheStringsAreInUpperCase() throws Exception {
-        List<String> namesCapitalized = Arrays.asList("John", "Steve", "Rene");
-        Iterator<String> namesCapitalizedIterator = namesCapitalized.iterator();
-        Flux<String> fluxWithNamesCapitalized = Flux.fromIterable(namesCapitalized);
+        List<String> namesCapitalized =
+                Arrays.asList("John", "Steve", "Rene");
+        Iterator<String> namesCapitalizedIterator =
+                namesCapitalized.iterator();
+        Flux<String> fluxWithNamesCapitalized =
+                Flux.fromIterable(namesCapitalized);
 
         Flux<String> fluxWithNamesInUpperCase = fluxWithNamesCapitalized
                 .map(name -> name.toUpperCase());
 
-        fluxWithNamesInUpperCase.subscribe(nameInUpperCase ->
-                Assert.assertEquals(namesCapitalizedIterator.next().toUpperCase(), nameInUpperCase)
+        fluxWithNamesInUpperCase.subscribe(nameInUpperCase -> {
+                    String expectedString =
+                            namesCapitalizedIterator.next().toUpperCase();
+
+                    Assert.assertEquals(expectedString, nameInUpperCase);
+                }
         );
     }
 }
