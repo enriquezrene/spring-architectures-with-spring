4b549b095272734bd3f46630752ffea1d3b8139b
Rene Enriquez <renriquez@ioet.com>
2018-03-16 18:24:14 -0500

    tests added

--- chapter05/banking-app/src/main/java/com/packtpub/bankingapp/integration/DefaultController.java
+++ chapter05/banking-app/src/main/java/com/packtpub/bankingapp/integration/DefaultController.java
@@ -1,4 +1,4 @@
-package com.packtpub.bankingapp.ui;
+package com.packtpub.bankingapp.integration;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
