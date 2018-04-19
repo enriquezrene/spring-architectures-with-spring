eedd91d629fd593f6ab6b1e14186aeda3488dd86
Rene Enriquez <enriquezrene.ap@gmail.com>
2018-04-19 10:42:35 -0500

    tests fixed

--- chapter06/event-sourcing/transfer-money-app/src/test/java/com/packtpub/eventsourcing/customer/CreateCustomerCommandTest.java
+++ chapter06/event-sourcing/transfer-money-app/src/test/java/com/packtpub/eventsourcing/customer/CreateCustomerCommandTest.java
@@ -1,12 +1,13 @@
 package com.packtpub.eventsourcing.customer;
 
-import com.google.gson.JsonObject;
 import com.packtpub.eventsourcing.commands.domain.CommandMetadata;
 import com.packtpub.eventsourcing.commands.persistence.CommandRepository;
 import com.packtpub.eventsourcing.customer.commands.CreateCustomerCommand;
-import com.packtpub.eventsourcing.customer.events.Event;
+import com.packtpub.eventsourcing.customer.events.AccountCreatedEvent;
 import com.packtpub.eventsourcing.customer.events.CustomerCreatedEvent;
 import com.packtpub.eventsourcing.customer.events.EventRepository;
+import com.packtpub.eventsourcing.events.EventProcessor;
+import org.json.simple.JSONObject;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -14,10 +15,10 @@ public class CreateCustomerCommandTest {
 
     @Test
     public void whenExecuteThenProduceACustomerCreatedEvent() throws Exception {
-        JsonObject createCustomerData = buildCreateCustomerData();
+        JSONObject createCustomerData = buildCreateCustomerData();
         CommandRepository commandRepository = Mockito.mock(CommandRepository.class);
         EventRepository eventRepository = Mockito.mock(EventRepository.class);
-        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository);
+        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository, Mockito.mock(EventProcessor.class));
 
         createCustomerCommand.execute();
 
@@ -27,35 +28,50 @@ public class CreateCustomerCommandTest {
 
     @Test
     public void whenExecuteThenProduceAnAccountCreatedEvent() throws Exception {
-        JsonObject createCustomerData = buildCreateCustomerData();
+        JSONObject createCustomerData = buildCreateCustomerData();
         CommandRepository commandRepository = Mockito.mock(CommandRepository.class);
         EventRepository eventRepository = Mockito.mock(EventRepository.class);
-        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository);
+        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository, Mockito.mock(EventProcessor.class));
 
         createCustomerCommand.execute();
 
-        Mockito.verify(eventRepository, Mockito.times(1)).save(Mockito.any(Event.class));
+        Mockito.verify(eventRepository, Mockito.times(1)).save(Mockito.any(AccountCreatedEvent.class));
     }
 
     @Test
     public void whenTheCommandIsExecutedItIsAlsoPersisted() throws Exception {
-        JsonObject createCustomerData = buildCreateCustomerData();
+        JSONObject createCustomerData = buildCreateCustomerData();
         CommandRepository commandRepository = Mockito.mock(CommandRepository.class);
         EventRepository eventRepository = Mockito.mock(EventRepository.class);
-        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository);
+        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository, Mockito.mock(EventProcessor.class));
 
         createCustomerCommand.execute();
 
         Mockito.verify(commandRepository, Mockito.times(1)).save(Mockito.any(CommandMetadata.class));
     }
 
+    @Test
+    public void whenTheCommandIsExecutedTheEventProcessTheEvents() throws Exception {
+        JSONObject createCustomerData = buildCreateCustomerData();
+        CommandRepository commandRepository = Mockito.mock(CommandRepository.class);
+        EventRepository eventRepository = Mockito.mock(EventRepository.class);
+        EventProcessor eventProcessor = Mockito.mock(EventProcessor.class);
+        CreateCustomerCommand createCustomerCommand = new CreateCustomerCommand(createCustomerData, commandRepository, eventRepository, eventProcessor);
 
-    private JsonObject buildCreateCustomerData() {
-        JsonObject createCustomerData = new JsonObject();
-        createCustomerData.addProperty("eventName", "Rene");
-        createCustomerData.addProperty("last_name", "Enriquez");
-        createCustomerData.addProperty("account_type", "savings");
-        createCustomerData.addProperty("initial_amount", 1000);
+        createCustomerCommand.execute();
+
+        Mockito.verify(eventProcessor, Mockito.times(1)).process(Mockito.any(AccountCreatedEvent.class));
+        Mockito.verify(eventProcessor, Mockito.times(1)).process(Mockito.any(CustomerCreatedEvent.class));
+    }
+
+
+
+    private JSONObject buildCreateCustomerData() {
+        JSONObject createCustomerData = new JSONObject();
+        createCustomerData.put("name", "Rene");
+        createCustomerData.put("last_name", "Enriquez");
+        createCustomerData.put("account_type", "savings");
+        createCustomerData.put("initial_amount", 1000);
         return createCustomerData;
     }
 }
